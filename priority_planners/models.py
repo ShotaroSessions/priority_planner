113bb4e0c895f9bd62897456957fbd583958206d
Shotaro Sessions <sessionsshotaro@gmail.com>
2020-03-04 13:42:32 -0500

    "Added update model"

--- priority_planners/models.py
+++ priority_planners/models.py
@@ -5,9 +5,22 @@ from colorful.fields import RGBColorField
 class Goal(models.Model):
     """A Goal the user wants to complete."""
     parent = None
-    children  = []
+    children = []
     color = RGBColorField()
     title = models.CharField(max_length=200)
     text = models.TextField()
     date_completed = None
 
+
+class Update(models.Model):
+    """A note regarding a specific goal."""
+    parent = models.ForeignKey(Goal, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        if len(self.text) > 40:
+            return f"{self.text[:50]}..."
+        else:
+            return self.text
