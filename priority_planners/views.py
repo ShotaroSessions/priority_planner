8264e927787392fb7cf50c2753f0de188c29c6d2
Shotaro Sessions <sessionsshotaro@gmail.com>
2020-03-05 23:35:56 -0500

    Added goals and index pages to views

--- priority_planners/views.py
+++ priority_planners/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Goal
+
+def index(request):
+    """The home page for Priority Planner."""
+    return render(request, 'priority_planners/index.html')
+
+def goals(request):
+    """Show all goals."""
+    goals = Goal.objects.order_by('date_added')
+    context = {'goals' : goals}
+    return render(request, 'priority_planners/goals.html', context)
